5b7c7de4d61985a9184596dd5d0ce34aae49ae58
JessAT18 <jessat18@outlook.com>
2023-07-10 22:11:03 -0400

    Paginación en las consultas

--- src/main/java/com/jessat/pizza/persistence/repository/PizzaPagSortRepository.java
+++ src/main/java/com/jessat/pizza/persistence/repository/PizzaPagSortRepository.java
@@ -4,4 +4,5 @@ import com.jessat.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+
 }
